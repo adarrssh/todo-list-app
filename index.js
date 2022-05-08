936e43e6ac39771b0fa75edbc127eff78d79e65f
adarrssh <adarsh00502@gmail.com>
2022-05-08 08:53:39 +0530

    done

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express")
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose")
+const TodoTask = require("./models/TodoTask")
+
+dotenv.config();
+app.use("/static", express.static("public"))
+const bodyParser = require("body-parser")
+
+app.use(express.urlencoded({extended:true}))
+
+mongoose.connect(process.env.DB_CONNECT, 
+{ useNewUrlParser: true,
+    useUnifiedTopology:true,
+
+}, () => {
+    console.log("Connected to db!");
+    app.listen(3000, () => console.log("Server Up and running"));
+});
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    TodoTask.find({},(err,tasks)=>{
+        res.render("todo.ejs",{todoTasks: tasks});
+    })
+})
+
+app.post("/", async (req, res) => {
+    
+    const todoTask = new TodoTask({
+        content: req.body.content
+    });
+    
+    // todoTask.save()
+    try {
+        await todoTask.save();
+        res.redirect("/")
+    } catch (error) {
+        console.log("error occured at indexjs line 40");
+        res.redirect("/")
+    }
+
+})
+
+app.route("/edit/:id")
+.get((req,res)=>{
+    const id = req.params.id;
+    TodoTask.find({},(err,tasks)=>{
+        res.render("todoEdit.ejs",{todoTasks:tasks,idTask:id});
+    });
+})
+.post((req,res)=>{
+    const id = req.params.id;
+
+    TodoTask.findByIdAndUpdate(id,{content:req.body.content},err=>{
+        if(err) return res.send(500,err);
+        res.redirect("/");
+    })
+})
+
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+    if (err) return res.send(500, err);
+    res.redirect("/");
+    });
+    });
+
